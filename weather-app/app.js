6abee933b7853d5d4f8c82c281f61ebc9aef787e
Ryan Hotton <ryanhotton@gmail.com>
2019-02-14 16:40:02 -0500

    S4 L31

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,9 +1,24 @@
 const request = require('request');
+const yargs = require('yargs');
 
-const urlAddress = 'http://www.mapquestapi.com/geocoding/v1/address?key=6z9N5TVsSK3ConRKYx7p1bHq9y8rncRm&location=1301%20lombard%20street%20philadelphia'
+const argv = yargs
+  .options({
+    a: {
+      demand: true,
+      alias: 'address',
+      describe: 'Address to fetch weather for',
+      string: true
+    }
+  })
+  .help()
+  .alias('help', 'h')
+  .argv;
+
+const urlAddress = 'http://www.mapquestapi.com/geocoding/v1/address?key=6z9N5TVsSK3ConRKYx7p1bHq9y8rncRm&location='
+var encodedAddress = encodeURIComponent(argv.address);
 
 request({
-  url: urlAddress,
+  url: urlAddress + encodedAddress,
   json: true
 }, (error, response, body) => {
   // Address
