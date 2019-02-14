cf18b58f415767b85bdecc2df9022de595c9a3e4
Ryan Hotton <ryanhotton@gmail.com>
2019-02-14 15:46:30 -0500

    S4 L30

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,12 +1,15 @@
 const request = require('request');
 
+const urlAddress = 'http://www.mapquestapi.com/geocoding/v1/address?key=6z9N5TVsSK3ConRKYx7p1bHq9y8rncRm&location=1301%20lombard%20street%20philadelphia'
+
 request({
-  url: 'http://www.mapquestapi.com/geocoding/v1/address?key=6z9N5TVsSK3ConRKYx7p1bHq9y8rncRm&location=1301%20lombard%20street%20philadelphia',
+  url: urlAddress,
   json: true
 }, (error, response, body) => {
-  console.log(JSON.stringify(body, undefined, 2));
+  // Address
+  console.log(`Address: ${body.results[0].locations[0].street}`);
   // Latitude
-  console.log('Latitude:', body.results[0].locations[0].latLng.lat);
+  console.log(`Latitude: ${body.results[0].locations[0].latLng.lat}`);
   // Longitude
-  console.log('Longitude:', body.results[0].locations[0].latLng.lng);
+  console.log(`Longitude: ${body.results[0].locations[0].latLng.lng}`);
 });
