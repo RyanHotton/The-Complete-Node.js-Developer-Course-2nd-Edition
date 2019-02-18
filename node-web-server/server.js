d3b79b17964cfd7f3a2b64d10469e44ac9b5abbd
Ryan Hotton <ryanhotton@gmail.com>
2019-02-18 17:03:23 -0500

    S5 L43

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,23 +1,25 @@
 const express = require('express');
+// for udemy course purposes only: high security vulnerability is known
+const hbs = require('hbs');
 
 var app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-  // res.send('<h1>Hello Express!</h1>');
-  res.send({
-    name: 'Ryan',
-    likes: [
-      'movies',
-      'games',
-      'technology'
-    ]
+  res.render('home.hbs', {
+    pageTitle: 'About Page',
+    welcomeMessage: 'Welcome to my website',
+    currentYear: new Date().getFullYear()
   });
 });
 
 app.get('/about', (req, res) => {
-  res.send('About page');
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+    currentYear: new Date().getFullYear()
+  });
 });
 
 // /bad
@@ -28,5 +30,5 @@ app.get('/bad', (req, res) => {
 });
 
 app.listen(3000, () => {
-  console.log('Server is up on port 3000.')
+  console.log('Server is up on port 3000.');
 });
