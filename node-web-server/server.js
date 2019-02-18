3484507b49ae0f2d2cf721c424088c16d9d42836
Ryan Hotton <ryanhotton@gmail.com>
2019-02-18 17:41:56 -0500

    S5 L45

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+
 const express = require('express');
 // for udemy course purposes only: high security vulnerability is known
 const hbs = require('hbs');
@@ -5,7 +7,27 @@ const hbs = require('hbs');
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials');
-app.set('view engine', 'hbs');
+app.set('view engine', 'hbs')
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+
+  next();
+});
+
+// // Maintenance middleware
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
