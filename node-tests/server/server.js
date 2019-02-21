eb7ea78c198f32fa4ad7f2299cd186e947ed8ee3
Ryan Hotton <ryanhotton@gmail.com>
2019-02-21 13:58:27 -0500

    S6 L56

--- node-tests/server/server.js
+++ node-tests/server/server.js
@@ -3,7 +3,26 @@ const express = require('express');
 var app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.status(404).send({
+    error: 'Page not found.',
+    name: 'Todo App v1.0'
+  });
+});
+
+app.get('/users', (req, res) => {
+  res.send([{
+    name: 'Ryan',
+    age: 24
+  }, {
+    name: 'Marcus',
+    age: 24
+  }, {
+    name: 'Donald',
+    age: 23
+  }
+  ]);
 });
 
 app.listen(3000);
+
+module.exports.app = app;
