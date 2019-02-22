e5ac6db0eea4252266da0fa0baa633d80708157c
Ryan Hotton <ryanhotton@gmail.com>
2019-02-22 10:53:26 -0500

    S7 L64

--- node-todo-api/playground/mongodb-connect.js
+++ node-todo-api/playground/mongodb-connect.js
@@ -1,4 +1,12 @@
-const MongoClient = require('mongodb').MongoClient;
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+var obj = new ObjectID();
+console.log(obj);
+
+// var user = {name: 'ryan', age: 24};
+// var {name} = user;
+// console.log(name);
 
 MongoClient.connect("mongodb://localhost:27017/TodoApp", { useNewUrlParser: true }, (err, client) => {
   if (err) {
@@ -18,16 +26,17 @@ MongoClient.connect("mongodb://localhost:27017/TodoApp", { useNewUrlParser: true
   // });
   //
 
-  // db.collection('Users').insertOne({
-  //   name: 'Ryan Hotton',
-  //   age: 24,
-  //   location: 'Montreal'
-  // }, (err, result) => {
-  //   if (err) {
-  //     return console.log('Unable to insert user', err);
-  //   }
-  //   console.log(JSON.stringify(result.ops, undefined, 2));
-  // });
+//  db.collection('Users').insertOne({
+//     name: 'Ryan Hotton',
+//     age: 24,
+//     location: 'Montreal'
+//   }, (err, result) => {
+//     if (err) {
+//       return console.log('Unable to insert user', err);
+//     }
+//     // console.log(JSON.stringify(result.ops, undefined, 2));
+//     console.log(result.ops[0]._id.getTimestamp());
+//   });
 
   client.close();
 });
