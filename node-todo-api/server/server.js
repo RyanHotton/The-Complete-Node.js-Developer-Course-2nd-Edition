c49de86a3a99533bc5b8d76e93d4e2a4e1e5e85a
Ryan Hotton <ryanhotton@gmail.com>
2019-02-25 15:17:52 -0500

    S7 L71

--- node-todo-api/server/server.js
+++ node-todo-api/server/server.js
@@ -5,25 +5,20 @@ mongoose.connect("mongodb://localhost:27017/TodoApp", { useNewUrlParser: true })
 
 var Todo = mongoose.model('Todo', {
   text: {
-    type: String
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    default: false
   },
   completedAt: {
-    type: Number
+    type: Number,
+    default: null
   }
 });
-
-// var newTodo = new Todo({
-//   text: 'Cook diner'
-// });
-
-// newTodo.save().then((doc) => {
-//   console.log('Saved todo', doc);
-// }, (e) => {
-//   console.log('Unable to save todo')
-// });
 
 // var otherTodo = new Todo({
 //   text: 'Feed the cat',
@@ -34,5 +29,26 @@ var Todo = mongoose.model('Todo', {
 // otherTodo.save().then((doc) => {
 //   console.log('Saved todo', doc);
 // }, (e) => {
-//   console.log('Unable to save todo')
+//   console.log('Unable to save todo', e);
 // });
+//
+
+// User model
+var User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  }
+});
+
+var user = new User({
+  email: 'ryan@example.com '
+});
+
+user.save().then((doc) => {
+  console.log('Saved user', doc);
+}, (e) => {
+  console.log('Unable to save user', e);
+});
